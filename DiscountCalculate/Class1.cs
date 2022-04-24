272015c1ca1a7243cbbc787fe8c5fc9570e4978a
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-24 12:31:38 +0530

    Add project files.

---
+++ DiscountCalculate/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiscountCalculate
+{
+    internal class Class1
+    {
+        public static void discount()
+        {
+            Console.WriteLine("Enter the amount of goods:");
+            int amount= int.Parse(Console.ReadLine());
+            int discount;
+
+            if (amount >= 30000)
+            {
+                discount = amount * 30 / 100;
+            }
+            else if (amount >=20000)
+            {
+                discount= amount * 20 / 100;
+            }
+            else if(amount >=10000)
+            {
+                discount= amount * 15 / 100;
+            }
+            else
+            {
+                discount= amount* 6/100;
+            }
+            Console.WriteLine("Congratulations....You Got Discount of :  {0} Rupees", discount);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DiscountCalculate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscountCalculate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.discount();
+        }
+    }
+}
